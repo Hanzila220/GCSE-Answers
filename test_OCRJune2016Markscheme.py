e6a74c9887200c3972752e9431d914e2c1f542f8
Hanzila220 <hanzila@dovidigital.com>
2024-11-28 19:39:54 +0500

    Update test_OCRJune2016Markscheme.py

--- test_OCRJune2016Markscheme.py
+++ test_OCRJune2016Markscheme.py
@@ -133,7 +133,7 @@ class TestWordpressLogin:
 
             except Exception as e:
                 # Capture any errors and log failure status
-                screenshot_path = f"screenshots/OCRJune20162017MarkScheme_error_{i+1}.png"
+                screenshot_path = f"screenshots/OCRJune2016MarkScheme_error_{i+1}.png"
                 self.driver.save_screenshot(screenshot_path)
 
                 results.append({
